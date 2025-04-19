606deb31c433bb3c3cfb15e5459fbafc3c295675
Krzysztof Józef Rutkowski <01192010@pw.edu.pl>
2025-04-19 10:41:51 +0200

    Add project files.

---
+++ src/Gra w życie.cpp
@@ -0,0 +1,12 @@
+﻿// Gra w życie.cpp : Defines the entry point for the application.
+//
+
+#include "Gra w życie.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
