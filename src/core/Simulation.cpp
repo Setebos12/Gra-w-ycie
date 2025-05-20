931136c2d1e661dffbea4dc16e27af0145ee7c14
Bartosz <bartoszpaszkiewicz05@gmail.com>
2025-05-20 20:32:34 +0200

    added board to simulation

--- src/core/Simulation.cpp
+++ src/core/Simulation.cpp
@@ -1,19 +1,23 @@
 #include "Simulation.h"
+#include "board.h"
 
 using namespace MVC;
 
 Simulation::Simulation()
     : input_(std::make_unique<Input>()),
-      logic_(std::make_unique<MVC::Logic>()),
-      render_(std::make_unique<MVC::Renderer>("Simulation",
-                                              sf::Vector2u{1920u, 1800u})) {}
+    logic_(std::make_unique<MVC::Logic>()),
+    render_(std::make_unique<MVC::Renderer>("Simulation",
+        sf::Vector2u{ 1920u, 1800u }))
+{
+    gameobjects_.emplace_back(std::make_unique<Board>("Game of Life Board", 100, 100));
+}
 
 void Simulation::run() {
-  while (true) {
-	//logic_->step(gameobjects_);
-    input_->pollEvents(window);
-    logic_->start();
-    render_->draw(gameobjects_);
-    input_->clear();
-  }
+    while (true) {
+        //logic_->step(gameobjects_);
+        input_->pollEvents(window);
+        logic_->start();
+        render_->draw(gameobjects_);
+        input_->clear();
+    }
 }
