8cec0789c397c1b6099a49d4b485331702475dde
Krzysztof Józef Rutkowski <01192010@pw.edu.pl>
2025-05-20 18:23:32 +0200

    ParseKeyboard Function

--- src/input/ParserKeyboard.cpp
+++ src/input/ParserKeyboard.cpp
@@ -1,3 +1,24 @@
 #pragma once
 
 #include "ParserKeyboard.h"
+
+
+constexpr int KEY_S = 83; // 'S' for Start
+constexpr int KEY_P = 80; // 'P' for Stop
+constexpr int KEY_E = 69; // 'E' for End
+constexpr int KEY_D = 68; // 'D' for ToggleDraw
+
+InputToken ParseKeyBoard::parseKey(const int key) const {
+    switch (key) {
+    case KEY_S:
+        return InputToken::Start;
+    case KEY_P:
+        return InputToken::Stop;
+    case KEY_E:
+        return InputToken::End;
+    case KEY_D:
+        return InputToken::ToggleDraw;
+    default:
+        return InputToken::Unknown;
+    }
+}
