5b018ccfd48b3cb314795ad798add90021f1f87a
Bartosz <bartoszpaszkiewicz05@gmail.com>
2025-05-20 19:24:15 +0200

    implemented board virtual methods

--- src/logic/board.cpp
+++ src/logic/board.cpp
@@ -1,5 +1,17 @@
 #include "board.h"
 #include <utility>
+#include <sstream>
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <string>
+#include <iostream>
+
+template<typename T>
+std::string toString(const T& value) {
+    std::ostringstream oss;
+    oss << value;
+    return oss.str();
+}
 
 Board::Board(const std::string& name, int width, int height)
 	: GameObject(name),
@@ -14,15 +26,21 @@ void Board::update()
 	int width = container->getWidth();
 	int height = container->getHeight();
 
+	int min_x = container->findMinMaxX().first;
+	int max_x = container->findMinMaxX().second;
+
+	int min_y = container->findMinMaxY().first;
+	int max_y = container->findMinMaxY().second;
+
 	std::vector<std::pair<int, int>> cellsToToggle;
 
-	for (int x = 0; x < width; ++x)
+	for (int x = min_x; x < max_x; ++x)
 	{
-		for (int y = 0; y < height; ++y)
+		for (int y = min_y; y < max_y; ++y)
 		{
 			if (container->getCellState(x, y) != pointHandle->shouldCellLive(x, y))
 			{
-				cellsToToggle.push_back({x, y});
+				cellsToToggle.push_back(std::make_pair(x, y));
 			}
 		}
 	}
@@ -54,4 +72,61 @@ int Board::getGenerationCount() const
 void Board::incrementGeneration()
 {
 	generationCount++;
+}
+
+void Board::draw(const Render::Drawer& drawer) {
+    if (!container) return;
+
+    float cellSize = 10.0f;
+
+    for (int x = 0; x < container->getWidth(); ++x) {
+        for (int y = 0; y < container->getHeight(); ++y) {
+            bool alive = container->getCellState(x, y);
+
+            sf::Vector2f position(x * cellSize, y * cellSize);
+            sf::Vector2f size(cellSize, cellSize);
+            sf::Color color = alive ? sf::Color::Green : sf::Color::Black;
+
+            drawer.drawRect(position, size, color);
+        }
+    }
+}
+
+std::string Board::printString() const {
+    std::string result;
+
+    result += toString(container->getWidth()) + " ";
+    result += toString(container->getHeight()) + " ";
+    result += toString(generationCount) + " ";
+
+    for (int x = 0; x < container->getWidth(); ++x) {
+        for (int y = 0; y < container->getHeight(); ++y) {
+            result += container->getCellState(x, y) ? '1' : '0';
+        }
+    }
+
+    return result;
+}
+
+void Board::readString(const std::string& read) {
+    std::istringstream iss(read);
+    int width, height, generation;
+    iss >> width >> height >> generation;
+
+    container = std::make_unique<Container>(width, height);
+    pointHandle = std::make_unique<PointHandle>(*container);
+    generationCount = generation;
+
+    std::string gridData;
+    iss >> gridData;
+
+    int index = 0;
+    for (int x = 0; x < width; ++x) {
+        for (int y = 0; y < height; ++y) {
+            if (index < gridData.size()) {
+                bool state = gridData[index++] == '1';
+                container->setCellState(x, y, state);
+            }
+        }
+    }
 }
