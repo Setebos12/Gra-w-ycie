34af72fe7e022edaa89f1fb1ca00e8ba654bf326
Krzysztof Józef Rutkowski <01192010@pw.edu.pl>
2025-05-24 11:47:06 +0200

    text function

--- src/render/drawer.cpp
+++ src/render/drawer.cpp
@@ -6,6 +6,8 @@
 
 #include "drawer.h"
 #include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/Text.hpp>
 
 using namespace Render;
 
@@ -22,7 +24,30 @@ void Drawer::drawRect(const sf::Vector2f &position, const sf::Vector2f &size,
   buffer_.emplace_back(std::move(rect));
 }
 
-void Drawer::drawText(const std::string &text) {}
+
+
+
+void Drawer::drawText(const std::string & textStr, const sf::Vector2f & position, unsigned int fontSize, const sf::Color & color) {
+    static sf::Font font;
+    static bool fontLoaded = false;
+
+    if (!fontLoaded) {
+        if (!font.openFromFile("../assets/arial.ttf")) {
+            return;
+        }
+        fontLoaded = true;
+    }
+
+    auto text{ std::make_unique<sf::Text>(font) };
+    text->setFont(font);
+    text->setString(textStr);
+    text->setCharacterSize(fontSize);
+    text->setFillColor(color);
+    text->setPosition(position);
+
+    buffer_.emplace_back(std::move(text));
+}
+
 
 void Drawer::refresh() {
   window_.clear();
